1d5fa253c5523c1d2bb3bfad85f232435710e37f
SHOPGEN431 <shopgenrep@gmail.com>
2025-09-01 18:44:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+
 import pandas as pd
 import os
 
